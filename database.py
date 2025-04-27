e8dbdc05742293aa408d8319f40c9d946d5e6f49
ruan65 <ruan65@gmail.com>
2025-04-28 00:37:58 +0300

    ready to connect to db

--- database.py
+++ database.py
@@ -2,5 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///quiz.db"
+DATABASE_URL = "postgresql://postgres:admin@localhost:5432/Quiz-app-tutorial"
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
 
